944d5db3a659432116d8e6164e3b4e68cff0b88e
hugobepa <hugo_bcn@hotmail.com>
2025-12-24 12:42:24 +0100

    MapApp_congifInicial

---
+++ app/permissions/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PermissionsScreen = () => {
+  return (
+    <View>
+      <Text>Permisos Screen</Text>
+    </View>
+  );
+};
+
+export default PermissionsScreen;
---
+++ manualReactNative/emulador/emulatorExtension.md
@@ -0,0 +1,4 @@
+emulator
+https://www.youtube.com/watch?v=fzJqHYjyA90
+https://www.youtube.com/watch?v=_bKd2yLGun8
+https://marketplace.visualstudio.com/items?itemName=Dannark.AndroidLauncher
